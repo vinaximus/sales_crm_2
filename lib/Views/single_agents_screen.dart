400f616e30ba78cdd014890aaee856c38d917910
Vinay <vinkandoi@gmail.com>
2025-07-06 22:56:34 +0530

    Started creating Single Agent Screen

--- lib/Views/single_agents_screen.dart
+++ lib/Views/single_agents_screen.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:sales_crm_2/Modal/agent.dart';
 
-class SingleAgentsScreen extends StatefulWidget{
-  SingleAgentsScreen({key}) {
-    key=super.key;
-  }
+enum formMode { addMode, editMode }
+
+class SingleAgentsScreen extends StatefulWidget {
+  SingleAgentsScreen({super.key, this.agentRecord, required this.mode})
+    : assert(
+        mode == formMode.addMode ||
+            (mode == formMode.editMode && agentRecord != null),
+        "agentRecord can't be null when mode is editmode",
+      );
+
+  Agent? agentRecord;
+  formMode mode;
 
   @override
   State<SingleAgentsScreen> createState() {
@@ -12,14 +21,73 @@ class SingleAgentsScreen extends StatefulWidget{
 }
 
 class _SingleAgentsScreenState extends State<SingleAgentsScreen> {
-  
+  final _form_key = GlobalKey<FormState>();
+
+  Agent? agentRecord;
+  @override
+  void initState() {
+    agentRecord = widget.agentRecord;
+    super.initState();
+  }
+
+  String agentName = "";
+
   @override
   Widget build(BuildContext context) {
-    return(
-    Scaffold(
+    return (Scaffold(
       appBar: (AppBar(
         backgroundColor: Colors.amber,
-        title: Text("Agent Details"),)),
-      body: Text("Agent View Single Window")));
+        title: Text("Agent Details"),
+      )),
+      body: Form(
+        key: _form_key,
+        child: Container(
+          padding: EdgeInsets.only(left: 50, right: 50),
+          child: Column(
+            children: [
+              Row(
+                spacing: 20,
+                children: [
+                  Icon(Icons.person),
+                  Expanded(
+                    child: TextFormField(
+                      initialValue: agentRecord.agentName,
+                      decoration: InputDecoration(label: Text("Agent Name")),
+                      validator: (value) {},
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                spacing: 20,
+                children: [
+                  Icon(Icons.phone),
+                  Expanded(
+                    child: TextFormField(
+                      initialValue: agentRecord.agentContactNumber,
+                      decoration: InputDecoration(label: Text("Contact No")),
+                      validator: (value) {},
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                spacing: 20,
+                children: [
+                  Icon(Icons.map),
+                  Expanded(
+                    child: TextFormField(
+                      initialValue: agentRecord.agentCity,
+                      decoration: InputDecoration(label: Text("City")),
+                      validator: (value) {},
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    ));
   }
 }
